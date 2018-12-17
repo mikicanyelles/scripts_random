d1cb6db33eb6bc44f494bfa84b1de004f182b310
Miki <miqeelcn@gmail.com>
2018-12-17 11:19:51 +0100

    Gantt script updated with new tasks

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ Sort of different scripts written mainly in Python or Shell which are useful for
 
 Takes the output of a QM/MM calculation from ChemShell (interfaced to either Turbomole or Gaussian) and plots the energies (QM/MM, QM and MM) along the optimisation of the structure or the optimisation of each point of a scan calculation.
 
+
+# gantt_phd.py
+
+Creates a Gantt Diagram using the python-gantt package.
+
 ## To-Do list
 - [ ] Add saving the pngs as an option (which can be modified modifing the script)
--- gantt_phd.py
+++ gantt_phd.py
@@ -42,19 +42,56 @@ p.add_task(p0), p.add_task(p1), p.add_task(p2)
 
 # MaR1
 ## 12-LOX + LTA4H
-### WT 12-LOX
+### WT 12-LOX (p000)
 
-p000t1 = gantt.Task(name='Abstraccions d\'hidrogen amb altres frames',
+p000.add_task(gantt.Task(name='Abstraccions d\'hidrogen amb altres frames',
                     start=dt.date(2018, 12, 31),
                     duration=60,
                     percent_done=10,
-                    resources=[rMCN],
-                    )
-p000.add_task(p000t1)
+                    resources=[rMCN]))
+
+
+### 2MT LTA4H (p001)
+
+p001t0 = gantt.Task(name='MD 2MT-LTA4H noEDH',
+                    start=dt.date(2018, 12, 17),
+                    duration=3,
+                    percent_done=25)
+                    #resources=[rMCN])
+p001.add_task(p001t0)
+    # Càlcul de la dinàmica i anàlisi d'aquesta. Es fa amb la intenció de generar 
+    # tants rotàmers com sigui possible de l'ARG382, per a després clusteritzar-los 
+    # i refer el docking amb el rotàmer més freqüent. A més a més, servirà per veure
+    # com evoluciona el Zn i la seva esfera de coordinació.
+
+p001t1 = gantt.Task(name='Redocking EDH a 2MT LTA4H',
+                    start=dt.date(2018, 12, 20),
+                    duration=4,                  
+                    percent_done=0)
+                    #depends_of=[p001t0])
+                    #resources=[rMCN])
+p001.add_task(p001t1)
+    # Un cop conegut el rotàmer més freqüent durant la dinàmica, s'utilitza com a
+    # estructura pel docking. 
+
+p001t2 = gantt.Task(name='Docking mutant alternatiu',
+                    start=dt.date(2018, 12, 17),
+                    duration=2,
+                    percent_done=0)
+p001.add_task(p001t2)
 
 
 
+
+
+
+
+
+
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
 p.make_svg_for_tasks(filename='test_phd.svg',
-                     today=dt.date(2018, 12, 12),
-                     start=dt.date(2018, 12, 10),
-                     end=dt.date(2018, 12, 31))
+                     today=dt.date(2018, 12, 14),
+                     start=dt.date(2018, 12, 1),
+                     end=dt.date(2018, 12, 31),
+                     scale=gantt.DRAW_WITH_DAILY_SCALE)
