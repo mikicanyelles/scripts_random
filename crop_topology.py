1001e681c7adec323f964f28df91583cc6a01bd2
Miquel Canyelles <mikicanyelles@qf-44-144.uab.es>
2020-02-06 16:31:27 +0100

    Modifications on crop_topology.py. Creation of weigths_list_creator.py. First steps on hessian_parser script

--- crop_topology.py
+++ crop_topology.py
@@ -11,12 +11,18 @@ import sys
 
 
 # File
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print('Input the topology/parameters and the coordinates as arguments. A name for the output file can be specified as the third argument (without an extension).')
+
+elif len(sys.argv) == 2:
+    name_pdb = sys.argv[1]
+    only_list = True
 
 elif len(sys.argv) >= 3:
     name_prmtop = sys.argv[1]
     #name_inpcrd = sys.argv[2]
+    only_list = False
+
     if str(sys.argv[2]).find('.pdb') == (len(sys.argv[2]) - 4):
         name_pdb = sys.argv[2]
         name_inpcrd = None
@@ -24,70 +30,77 @@ elif len(sys.argv) >= 3:
         name_inpcrd = sys.argv[2]
         name_pdb = None
 
+if only_list == False:
+    # Ask for Ligand and Radius
+    ligand = input('Type the name or the three letters code (only if it is a non-standard residue) of the central ligand: ')
+    while True:
+        quest = input('Is %s correct ([y]/n)? ' % ligand)
+        if quest in ('', 'y', 'Y', 'yes', 'YES', 'Yes', 'yES', 'YEs', 'yeS', 'yEs', 'YeS'):
+            break
+        elif quest in ('no', 'NO', 'No', 'nO'):
+            ligand = input('Type the name or the three letters code (only if it is a non-standard residue) of the central ligand: ')
+            continue
+        else :
+            print('Answer \'yes\' or \'no\'.')
 
-# Ask for Ligand and Radius
-ligand = input('Type the name or the three letters code (only if it is a non-standard residue) of the central ligand: ')
-while True:
-    quest = input('Is %s correct ([y]/n)? ' % ligand)
-    if quest in ('', 'y', 'Y', 'yes', 'YES', 'Yes', 'yES', 'YEs', 'yeS', 'yEs', 'YeS'):
-        break
-    elif quest in ('no', 'NO', 'No', 'nO'):
-        ligand = input('Type the name or the three letters code (only if it is a non-standard residue) of the central ligand: ')
-        continue
-    else :
-        print('Answer \'yes\' or \'no\'.')
 
-
-while True:
-    try:
-        radius = float(input('Which is the desired radius for the water drop (in Å)? '))
-        break
-    except ValueError:
-        print('Type only a number.')
-        continue
-while True:
-    quest = input('Is %s correct ([y]/n)? ' % radius)
-    if quest in ('', 'y', 'Y', 'yes', 'YES', 'Yes', 'yES', 'YEs', 'yeS', 'yEs', 'YeS'):
-        break
-    elif quest in ('no', 'NO', 'No', 'nO'):
-        radius = input('Which is the desired radius for the water drop (in  Å)? ')
-        continue
-    else :
-        print('Answer \'yes\' or \'no\'.')
- 
-# Output files names
-
-if len(sys.argv) < 4:
-    name_prmtop_out = name_prmtop[:-7] + '.cropped.prmtop'
-    if name_inpcrd == None:
-        name_inpcrd_out = name_prmtop[:-7] + '.cropped.inpcrd'
-    else :
-        name_inpcrd_out = name_inpcrd[:-7] + '.cropped.inpcrd'
-
-elif len(sys.argv) == 4:
-    name_prmtop_out = sys.argv[3] + '.prmtop'
-    name_inpcrd_out = sys.argv[3] + '.inpcrd'
+    while True:
+        try:
+            radius = float(input('Which is the desired radius for the water drop (in Å)? '))
+            break
+        except ValueError:
+            print('Type only a number.')
+            continue
+    while True:
+        quest = input('Is %s correct ([y]/n)? ' % radius)
+        if quest in ('', 'y', 'Y', 'yes', 'YES', 'Yes', 'yES', 'YEs', 'yeS', 'yEs', 'YeS'):
+            break
+        elif quest in ('no', 'NO', 'No', 'nO'):
+            radius = input('Which is the desired radius for the water drop (in  Å)? ')
+            continue
+        else :
+            print('Answer \'yes\' or \'no\'.')
     
+    # Output files names
 
-# Crop the system
+    if len(sys.argv) < 4:
+        name_prmtop_out = name_prmtop[:-7] + '.cropped.prmtop'
+        if name_inpcrd == None:
+            name_inpcrd_out = name_prmtop[:-7] + '.cropped.inpcrd'
+        else :
+            name_inpcrd_out = name_inpcrd[:-7] + '.cropped.inpcrd'
 
-if name_pdb != None:
-    topology = load_file(name_prmtop, xyz=name_pdb)
-elif name_inpcrd != None:
-    topology = load_file(name_prmtop, xyz=name_inpcrd)
-topology.box = None
-topology.strip(':WAT&:%s<@%s' % (ligand, radius))
-topology.strip(':Na+&:%s<@%s' % (ligand, radius))
-topology.strip(':Cl-&:%s<@%s' % (ligand, radius))
+    elif len(sys.argv) == 4:
+        name_prmtop_out = sys.argv[3] + '.prmtop'
+        name_inpcrd_out = sys.argv[3] + '.inpcrd'
+        
 
-topology.write_parm(name_prmtop_out)
-topology.save(name_inpcrd_out)
+    # Crop the system
 
-print('Cropped topology and coordinates have been saved as \'%s\' and \'%s\'' % (name_prmtop_out, name_inpcrd_out))
-##################
+    if name_pdb != None:
+        topology = load_file(name_prmtop, xyz=name_pdb)
+    elif name_inpcrd != None:
+        topology = load_file(name_prmtop, xyz=name_inpcrd)
+    topology.box = None
+    topology.strip(':WAT&:%s<@%s' % (ligand, radius))
+    topology.strip(':Na+&:%s<@%s' % (ligand, radius))
+    topology.strip(':Cl-&:%s<@%s' % (ligand, radius))
+
+    topology.write_parm(name_prmtop_out)
+    topology.save(name_inpcrd_out)
+
+    print('Cropped topology and coordinates have been saved as \'%s\' and \'%s\'' % (name_prmtop_out, name_inpcrd_out))
+    ##################
+
 
 # Generate active atoms list
-u_set_act = Universe(name_prmtop_out, name_inpcrd_out)
+if only_list == False:
+    u_set_act = Universe(name_prmtop_out, name_inpcrd_out)
+    name = name_prmtop_out[:-7]
+elif only_list == True:
+    u_set_act = Universe(name_pdb)
+    name = name_pdb[:-4]
+    radius = 100000000
 
 while True:
     try :
@@ -136,7 +149,8 @@ while True:
         continue
 
 selection = u_set_act.select_atoms(str('byres around %s bynum %s' % (radius_set_act, carbon)))
-txt = open('set_act_%s_%s' % (name_prmtop_out[:-7], radius_set_act), 'w')
+
+txt = open('set_act_%s_%s' % (name, radius_set_act), 'w')
 txt.write('set act { ')
 for i in range(0, len(selection)):
     locA = str(selection[i]).find('<Atom ') + 6
---
+++ weigths_list_creator.py
@@ -0,0 +1,137 @@
+from os import remove, path
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser(description="Simple script for creating lists of weights for some ChemShell calculations")
+
+parser.add_argument(
+    '-act',
+    help='tcl list of the active atoms. This will be the reference for building the list of weights.',
+    required=True
+    )
+parser.add_argument(
+    '-qm',
+    help='tcl list of the QM atoms. This is the second list, not necessarily the QM list',
+    required=True
+    )
+
+parser.add_argument(
+    '-core',
+    help='tcl list of the core atoms. This is thought to be a little list of greater weight than the QM atoms',
+    required=False,
+    )
+parser.add_argument(
+    '-act_w', '--act_weigth',
+    help='Float value for the active atoms weigth (the non QM nor core). The default value is 0.0',
+    required=False,
+    default=0.0
+    )
+parser.add_argument(
+    '-qm_w', '--qm_weigth',
+    help='Float value for the QM atoms weigth. The default value is 1.0',
+    required=False,
+    default=1.0
+    )
+parser.add_argument(
+    '-core_w', '--core_weigth',
+    help='Float value for the core atoms weigth. The default value is 2.0',
+    required=False,
+    default=1.0
+    )
+
+parser.add_argument(
+    '-of', '--output_file_name',
+    help='Name of the output file name',
+    required=False,
+    )
+
+argsdict = vars(parser.parse_args())
+
+try :
+    float(argsdict['act_weigth'])
+    float(argsdict['qm_weigth'])
+    float(argsdict['core_weigth'])
+except ValueError:
+    print('The specified weights are not float numbers. Rerun the scripts specifying the weights in the proper way.')
+    exit()
+
+
+
+def list_from_file(filename):
+    file_ = open(filename).readlines()[0].split()
+    list_ = []
+
+    for i in range(len(file_)):
+        try :
+            list_.append(int(file_[i]))
+        except ValueError:
+            pass
+
+    return list_
+print(argsdict['act'])
+
+list_act = list_from_file(argsdict['act'])
+list_qm = list_from_file(argsdict['qm'])
+
+if argsdict['core'] != None:
+    list_ts = list_from_file(argsdict['core'])
+
+
+if argsdict['output_file_name'] == None:
+    filename = ''
+    if len(argsdict['act'].split('/')) ==  1:#and argsdict['act'].split('/')[0] == '':
+        filename = 'weigths_' + str(argsdict['act'])
+        
+    elif len(argsdict['act'].split('/')) > 1:
+        for i in range(0,len(argsdict['act'].split('/'))-1):
+            filename = filename + str(argsdict['act'].split('/')[i]) + '/'
+            
+
+        filename = filename + 'weigths_' + str(argsdict['act'].split('/')[-1])
+
+else:
+    filename = argsdict['output_file_name']
+
+try:
+    f = open(filename)
+    exists = True
+    # Do something with the file
+except IOError:
+    print("File not accessible")
+finally:
+    f.close()
+
+if exists == True:
+    while True:
+        quest = input('\'%s\' existis. Do you want to remove it ([y]/no)? ' % filename)
+        if quest.lower() in ('', 'y', 'yes', '1'):
+            remove(filename)
+            break
+        elif quest.lower() in ('n', 'no', '0'):
+            filename = input('Specify the new name for the output file: ')
+            print('The new file will be saved as %s' % filename)
+            break
+        else :
+            print('Please, answer \'yes\' or \'no\'.')
+
+weights = open(filename, 'w')
+
+weights.write('set weights {')
+for i in list_act:
+    if argsdict['core'] != None:
+        if i in list_qm and i not in list_ts:
+            weights.write(' ' + str(argsdict['qm_weigth']))
+        elif i in list_ts:
+            weights.write(' ' + str(argsdict['core_weigth']))
+        else:
+            weights.write(' ' + str(argsdict['act_weigth']))
+    elif argsdict['core'] == None:
+        if i in list_qm:
+            weights.write(' ' + str(argsdict['qm_weigth']))
+        else:
+            weights.write(' ' + str(argsdict['act_weigth']))
+
+
+
+weights.write(' }')
+weights.close()
