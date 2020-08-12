f4e425bb6f0dd65fbf25375f85b1e1c7a5189f89
Miquel Canyelles <miqeelcn@gmail.com>
2020-08-12 12:07:35 +0200

    Updated the script adding the possibility to specify the name of the output pdb. It has been also improved by splitting the script into two functions (parser and main) and adding the __name__ checker.

--- atoms_list_to_pdb.py
+++ atoms_list_to_pdb.py
@@ -1,40 +1,55 @@
 # coding: utf-8
 
-import MDAnalysis as mda
-import sys, os
+from MDAnalysis import Universe
+from sys import getrecursionlimit, argv
 
-list_name  = sys.argv[1]
-pdb_name = sys.argv[2]
+def parser():
+    list_name  = argv[1]
+    pdb_name = argv[2]
 
-atoms_list = open(list_name, 'r').readlines()
-
-atoms_num = []
-
-for i in range(len(atoms_list)):
-    atoms_ar = atoms_list[i].split()
-
-    for j in range(len(atoms_ar)):
-        try :
-            atoms_num.append(int(atoms_ar[j]))
-        except ValueError:
-            pass
-
-print('The zone has %i atoms (if it is a QM zone, link atoms are not counted)' % len(atoms_num))
-
-u = mda.Universe(pdb_name)
-
-atoms_sel_list = ''
-
-if sys.getrecursionlimit() < len(atoms_num):
-    sys.setrecursionlimit(len(atoms_num) + 1000)
-
-for i in range(len(atoms_num)):
-    if i == len(atoms_num) - 1:
-        atoms_sel_list = atoms_sel_list + 'bynum ' + str(atoms_num[i])
+    if len(argv) == 4:
+        out_name = argv[3]
     else :
-        atoms_sel_list = atoms_sel_list + 'bynum ' + str(atoms_num[i]) + ' or '
+        out_name = list_name
 
-atoms_sel = u.select_atoms(atoms_sel_list)
+    return list_name, pdb_name, out_name
 
-#atoms_sel.write(str(os.getcwd())+ list_name + '.pdb')
-atoms_sel.write(list_name + '.pdb')
+def main(list_name, pdb_name, out_name):
+
+    atoms_list = open(list_name, 'r').readlines()
+
+    atoms_num = []
+
+    for i in range(len(atoms_list)):
+        atoms_ar = atoms_list[i].split()
+
+        for j in range(len(atoms_ar)):
+            try :
+                atoms_num.append(int(atoms_ar[j]))
+            except ValueError:
+                pass
+
+    print('The zone has %i atoms (if it is a QM zone, link atoms are not counted)' % len(atoms_num))
+
+    u = Universe(pdb_name)
+
+    atoms_sel_list = ''
+
+    if getrecursionlimit() < len(atoms_num):
+        setrecursionlimit(len(atoms_num) + 1000)
+
+    for i in range(len(atoms_num)):
+        if i == len(atoms_num) - 1:
+            atoms_sel_list = atoms_sel_list + 'bynum ' + str(atoms_num[i])
+        else :
+            atoms_sel_list = atoms_sel_list + 'bynum ' + str(atoms_num[i]) + ' or '
+
+    atoms_sel = u.select_atoms(atoms_sel_list)
+
+    atoms_sel.write(out_name + '.pdb')
+
+
+if __name__ == '__main__':
+    list_name, pdb_name, out_name = parser()
+    main(list_name, pdb_name, out_name)
+
