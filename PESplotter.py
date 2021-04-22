71aa16a3ad16b3219b75998fdfe8ed6c1f124d06
Miquel Canyelles <miqeelcn@gmail.com>
2021-04-22 10:39:58 +0200

    Created a script for plotting the PES of a scan calculation. Matplotlib output is not implemented yet.

---
+++ PESplotter.py
@@ -0,0 +1,163 @@
+import matplotlib.pyplot as plt
+from os import listdir, environ
+from sys import argv, exit
+import plotext.plot as plx
+from termcolor import colored
+import numpy as np
+
+def input_file():
+    """
+    DESCRIPTION
+        Function for setting the file_name. It can be the default (PES.plt) or an input file. It is checked if the file is in the directory and the program is quitted if it isn't.
+    """
+    if len(argv) == 1:
+        if 'PES.plt' in listdir():
+            file_name = 'PES.plt'
+            break
+
+        else :
+            print('No PES.plt file in the directory. Please, specify the filename as an argument')
+            exit()
+
+    elif len(argv) == 2:
+        if argv[1] in listdir():
+            file_name = argv[1]
+            break
+
+        else :
+            print(file_name, 'does not exist in the directory.')
+            exit()
+
+    return file_name
+
+
+def parse_file(file_name):
+    df = pd.DataFrame(pd.read_csv(file_name))
+
+    for key in df.keys():
+        x_key = []
+        if 'rc' in key:
+            x_key.append(key)
+        elif 'deltaE' in key:
+            y_key.append(key)
+        else :
+            pass
+
+    if len(x_key) > 1:
+        print('Detected possible coordinates:')
+        for key in range(len(x_key)):
+            print(key + 1, '. ', x_key[key])
+
+        while True:
+            try :
+                quest = int(input('Which coordinate do you want? '))
+
+                if quest in range(1, len(x_key) + 1):
+                    x_key = x_key[quest-1]
+                    break
+
+                else :
+                    print('The selected coordinate does not exist.')
+                    continue
+
+            except ValueError:
+                print('Type a number')
+                continue
+
+    elif len(x_key) == 1:
+        x_key = x_key[0]
+
+    elif len(x_key) == 0:
+        print('No reaction coordinate has been detected. Please, select one of the following: ')
+        for k in range(len(df.keys())):
+            print(k+1, ': ', df.keys()[k])
+
+        while True:
+            try :
+                quest = int(input('Select the number of the column containing the reaction coordinate: ')
+
+                if quest in range(1, len(x_key) + 1):
+                    x_key = x_key[quest-1]
+                    break
+
+                else :
+                    print('The selected column does not exist.')
+                    continue
+
+            except ValueError:
+                print('Type a number')
+                continue
+
+    x = list(df[key])
+
+
+    if len(y_key) > 1:
+        print('Detected possible relative energy:')
+        for key in range(len(x_key)):
+            print(key + 1, '. ', y_key[key])
+
+        while True:
+            try :
+                quest = int(input('Which energy do you want? '))
+
+                if quest in range(1, len(y_key) + 1):
+                    y_key = y_key[quest-1]
+                    break
+
+                else :
+                    print('The selected coordinate does not exist.')
+                    continue
+
+            except ValueError:
+                print('Type a number')
+                continue
+
+    elif len(y_key) == 1:
+        y_key = y_key[0]
+
+    elif len(y_key) == 0:
+        print('No relative energy has been detected. Please, select one of the following: ')
+        for k in range(len(df.keys())):
+            print(k+1, ': ', df.keys()[k])
+
+        while True:
+            try :
+                quest = int(input('Select the number of the column containing the relative energy: ')
+
+                if quest in range(1, len(y_key) + 1):
+                    y_key = y_key[quest-1]
+                    break
+
+                else :
+                    print('The selected column does not exist.')
+                    continue
+
+            except ValueError:
+                print('Type a number')
+                continue
+
+    y = list(df[key])
+
+    return x,y
+
+def plot_energy_plx(x,y):
+    plx.plot(x,y)
+    plx.show()
+    print(colored(str(len(x)), ' points of the scan have been calculated so far.', 'red'))
+
+
+def main():
+
+    file_name = input_file():
+    x,y = parse_file(file_name)
+
+    try :
+        environ['DISPLAY']
+        #plot_energy_mtl(x,y)
+
+    except KeyError:
+        plot_energy_plx(x,y)
+
+
+if __name__ == '__main__':
+    main()
