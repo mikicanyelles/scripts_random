af8b12d6a638a8d2bfa3478596b3267abfa8a0c2
Miquel Canyelles <miqeelcn@gmail.com>
2022-03-23 12:15:16 +0100

    Script for performing RMSD of CA's of a given trajectory.

---
+++ rmsd_CA.py
@@ -0,0 +1,47 @@
+import re
+from MDAnalysis import Universe
+import MDAnalysis.analysis.rms as rms
+import argparse
+import  matplotlib.pyplot as plt
+import re
+
+def natural_sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
+
+parser = argparse.ArgumentParser(description='Script for performing RMSD of the CA for a given trajectory.')
+
+parser.add_argument(
+    '-p', '--parameters',
+    help='Flag for specify the parameters in any format compatible with MDAnalysis (prmtop, for example).',
+    required=True
+)
+
+parser.add_argument(
+    '-t', '--trajectory',
+    help='Flag for specify the trajectory or trajectories files.',
+    required=True,
+    nargs='+'
+)
+
+argsdict = vars(parser.parse_args())
+
+if len(argsdict['trajectory']) > 1:
+    argsdict['trajectory'] = natural_sort(argsdict['trajectories'])
+
+u = Universe(argsdict['parameters'], argsdict['trajectory'])
+
+R = rms.RMSD(u,
+    select='name CA',
+    )
+R.run()
+
+rmsd = R.results.rmsd.T
+time = rmsd[1]
+
+plt.plot(time, rmsd[2])
+plt.xlabel('Time (ns)')
+plt.ylabel('RMSD (Å)')
+plt.show()
+plt.close()
